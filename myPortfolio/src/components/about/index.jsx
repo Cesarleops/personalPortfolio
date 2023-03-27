e7b33a5f18c8f627068afd1e7443d1ced6587416
Cesar Leyton <cesarleyton549@gmail.com>
2023-03-27 10:26:22 -0500

    little fix

--- myPortfolio/src/components/about/index.jsx
+++ myPortfolio/src/components/about/index.jsx
@@ -6,7 +6,7 @@ export const AboutMe = () => {
         <section className="aboutSection">
             <h2 className='aboutSection--title'>Some things about me</h2>
             <Fade>
-                <p className='aboutSection--info'>I'm currently studying software engineering at Universidad Nacional in Medellin, and I've been working as a volunteer front-end developer at Umoob, a fitness Start Up. I really enjoy working as a member of a team, I like to share ideas and work with a common objective, I enjoy solving problems and learning new things constantly.
+                <p className='aboutSection--info'>I'm currently studying software engineering at Universidad Nacional in Medellin, and I've been working as a front-end developer at Umoob, a fitness Start Up. I really enjoy working as a member of a team, I like to share ideas and work with a common objective, I enjoy solving problems and learning new things constantly.
                     Besides of work and studies I like to read novels and books related with space, I also like to play a lot of games.
                 </p>
             </Fade>
