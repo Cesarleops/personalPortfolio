95eb4e566614dd4b793993604d5afeb269166560
Cesar Leyton <cesarleyton549@gmail.com>
2023-05-04 16:01:38 -0500

    Fixed images bugs

--- myPortfolio/src/components/mobileProjects/index.jsx
+++ myPortfolio/src/components/mobileProjects/index.jsx
@@ -6,8 +6,8 @@ const myProjects = {
         title: "Sign Up form",
           desc: "A funny challenge from Front-End Mentor where you have to develop a responsive form for a games suscription with a stepper.",
           stack: ["Typescript", "React", "Scss"],
-          image: ["src/assets/firsts.png", "src/assets/seconds.png", "src/assets/thirds.png",
-                    "src/assets/fo.png", "src/assets/fifths.png", "src/assets/six.png", "src/assets/seven.png", "src/assets/eight.png"],
+          image: ["../../../public/assets/firsts.png", "../../../public/assets/seconds.png", "../../../public/assets/thirds.png",
+                    "../../../public/assets/fo.png", "../../../public/assets/fifths.png", "../../../public/assets/six.png", "../../../public/assets/seven.png", "../../../public/assets/eight.png"],
           gitHub: "https://github.com/Cesarleops/advancedSignUp",
           demo: "https://neon-mousse-70d49d.netlify.app"
     },
@@ -16,7 +16,7 @@ const myProjects = {
           title: "AllGames",
           desc: "A non-responsive library where you can find information about any video game such as description, release date, reviews and more. You can see the most popular games as well as use filters to simplify your search.",
           stack: ["Javascript", "React", "Redux", "CSS", "PostgreSql", "Express"],
-          image: ["src/assets/all1.png", "src/assets/all2.png","src/assets/all3.png","src/assets/all4.png", ],
+          image: ["../../../public/assets/all1.png", "../../../public/assets/all2.png","../../../public/assets/all3.png","../../../public/assets/all4.png", ],
           gitHub: "https://github.com/Cesarleops/PI-Videogames",
           demo: "https://pi-videogames-luh0u0eyu-cesarleops.vercel.app"
   
@@ -25,7 +25,7 @@ const myProjects = {
           title: "H-BUY",
           desc: "E-Commerce developed as a team, we used the MERN stack to develop an E-commerce that was functional for different industries, it has authorization, payment gateway, administrative profiles and other functionalities. ",
           stack: ["Javascript", "React", "Redux", "Bootstrap", "MongoDB", "MaterialUI", "Express"],
-          image: ["src/assets/Captura de Pantalla 2023-05-04 a la(s) 7.56.13 a.m..png"],
+          image: ["../../../public/assets/Captura de Pantalla 2023-05-04 a la(s) 7.56.13 a.m..png"],
           gitHub: "https://github.com/Felipesch96/PF-H-Buy",
           demo: "https://main.d2d0y3pf0pfssa.amplifyapp.com/"
 
@@ -34,7 +34,7 @@ const myProjects = {
           title: "Umoob",
           desc: "Umoob is a business management app and a marketplace focused on the fitness industry. I've worked as a front-end developer in several development cycles ",
           stack: ["Typescript", "Python", "React", "Redux" ,"Sass", "Django"],
-          image: ["https://cesarroom.netlify.app/src/assets/imagen.png", "https://cesarroom.netlify.app/src/assets/umoob2.png", "https://cesarroom.netlify.app/src/assets/umoob3.png"],
+          image: ["../../../public/assets/imagen.png", "../../../public/assets/umoob2.png", "../../../public/assets/umoob3.png"],
           demo: "https://www.umoob.com/"
   
           }
