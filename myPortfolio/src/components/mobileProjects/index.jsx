4005cd47d0f56e5ed46b56a64a632ed1b00d1a75
Cesar Leyton <cesarleyton549@gmail.com>
2023-05-04 15:48:13 -0500

    Fixing images links

--- myPortfolio/src/components/mobileProjects/index.jsx
+++ myPortfolio/src/components/mobileProjects/index.jsx
@@ -34,7 +34,7 @@ const myProjects = {
           title: "Umoob",
           desc: "Umoob is a business management app and a marketplace focused on the fitness industry. I've worked as a front-end developer in several development cycles ",
           stack: ["Typescript", "Python", "React", "Redux" ,"Sass", "Django"],
-          image: ["/Users/cesarleyton/Desktop/personalPortfolio/myPortfolio/src/assets/imagen.png", "src/assets/umoob2.png", "src/assets/umoob3.png"],
+          image: ["../../assets/imagen.png", "../../assets/umoob2.png", "../../assets/umoob3.png"],
           demo: "https://www.umoob.com/"
   
           }
