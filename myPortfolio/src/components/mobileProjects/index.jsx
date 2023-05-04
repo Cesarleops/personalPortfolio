6685e07a8cb3cd435d4de673fbf4ab58490a9fe8
Cesar Leyton <cesarleyton549@gmail.com>
2023-05-04 16:25:53 -0500

    Changed images imports

--- myPortfolio/src/components/mobileProjects/index.jsx
+++ myPortfolio/src/components/mobileProjects/index.jsx
@@ -1,15 +1,28 @@
 import { ProjectsCard } from '../ProjectsCard'
 import './mobileProjects.scss'
-import Hero from '../../../public/assets/firsts.png'
-import Two from '../../../public/assets/seconds.png'
-import Three from '../../../public/assets/thirds.png'
+import FirstPic from '../../../public/assets/firsts.png'
+import SecondPic from '../../../public/assets/seconds.png'
+import ThirdPic from '../../../public/assets/thirds.png'
+import FourthPic from  '../../../public/assets/fo.png'
+import FifthPic from  '../../../public/assets/fifths.png'
+import SixthPic from  '../../../public/assets/six.png'
+import SeventhPic from  '../../../public/assets/seven.png'
+import EighthPic from  '../../../public/assets/eight.png'
+import AllOne from '../../../public/assets/all1.png'
+import AllTwo from '../../../public/assets/all2.png'
+import AllThree from '../../../public/assets/all3.png'
+import AllFourth from '../../../public/assets/all4.png'
+import HBuy from '../../../public/assets/Captura de Pantalla 2023-05-04 a la(s) 7.56.13 a.m..png'
+import UmoobOne from '../../../public/assets/imagen.png'
+import UmoobTwo from '../../../public/assets/umoob2.png'
+import UmoobThree from '../../../public/assets/umoob3.png'
 const myProjects = {
     AdvancedSignUp: {
         title: "Sign Up form",
           desc: "A funny challenge from Front-End Mentor where you have to develop a responsive form for a games suscription with a stepper.",
           stack: ["Typescript", "React", "Scss"],
-          image: [Hero, Two, Three,
-                    "../../../public/assets/fo.png", "../../../public/assets/fifths.png", "../../../public/assets/six.png", "../../../public/assets/seven.png", "../../../public/assets/eight.png"],
+          image: [FirstPic, SecondPic, ThirdPic,
+                    FourthPic, FifthPic,SixthPic, SeventhPic, EighthPic],
           gitHub: "https://github.com/Cesarleops/advancedSignUp",
           demo: "https://neon-mousse-70d49d.netlify.app"
     },
@@ -18,7 +31,7 @@ const myProjects = {
           title: "AllGames",
           desc: "A non-responsive library where you can find information about any video game such as description, release date, reviews and more. You can see the most popular games as well as use filters to simplify your search.",
           stack: ["Javascript", "React", "Redux", "CSS", "PostgreSql", "Express"],
-          image: ["../../../public/assets/all1.png", "../../../public/assets/all2.png","../../../public/assets/all3.png","../../../public/assets/all4.png", ],
+          image: [AllOne, AllTwo, AllThree, AllFourth ],
           gitHub: "https://github.com/Cesarleops/PI-Videogames",
           demo: "https://pi-videogames-luh0u0eyu-cesarleops.vercel.app"
   
@@ -27,7 +40,7 @@ const myProjects = {
           title: "H-BUY",
           desc: "E-Commerce developed as a team, we used the MERN stack to develop an E-commerce that was functional for different industries, it has authorization, payment gateway, administrative profiles and other functionalities. ",
           stack: ["Javascript", "React", "Redux", "Bootstrap", "MongoDB", "MaterialUI", "Express"],
-          image: ["../../../public/assets/Captura de Pantalla 2023-05-04 a la(s) 7.56.13 a.m..png"],
+          image: [HBuy],
           gitHub: "https://github.com/Felipesch96/PF-H-Buy",
           demo: "https://main.d2d0y3pf0pfssa.amplifyapp.com/"
 
@@ -36,7 +49,7 @@ const myProjects = {
           title: "Umoob",
           desc: "Umoob is a business management app and a marketplace focused on the fitness industry. I've worked as a front-end developer in several development cycles ",
           stack: ["Typescript", "Python", "React", "Redux" ,"Sass", "Django"],
-          image: ["../../../public/assets/imagen.png", "../../../public/assets/umoob2.png", "../../../public/assets/umoob3.png"],
+          image: [UmoobOne, UmoobTwo, UmoobThree],
           demo: "https://www.umoob.com/"
   
           }
